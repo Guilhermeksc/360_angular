c252cc9dc3331b55d437d47589dff8f2be0df1d5
GuilhermeK <guilhermekscampos@gmail.com>
2024-12-20 00:12:21 -0300

    att

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Inicialização do vídeo após o DOM ser carregado
+document.addEventListener('DOMContentLoaded', () => {
+  const videoElement = document.querySelector('header.masthead video') as HTMLVideoElement;
+  if (videoElement) {
+    videoElement.muted = true; // Garante que o autoplay seja permitido
+    videoElement.play().catch((error) => {
+      console.error('Não foi possível iniciar o vídeo automaticamente:', error);
+    });
+  }
+});
+
+// Inicialização do Angular
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes), // Passa as rotas configuradas
+    provideHttpClient(), // Configuração global do HttpClient com interceptores
+  ],
+}).catch(err => console.error(err));
